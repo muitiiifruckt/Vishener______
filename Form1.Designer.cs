2ca02d9cbe8b4ef99b51062f016dcba939d0c8b7
multifrukt <multifrukt@MULTIFRUKT>
2023-02-24 19:33:33 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Первая_пара_тимп
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encryption = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1267, 744);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.encryption);
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Controls.Add(this.richTextBox3);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1259, 715);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Шифровать";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ключ";
+            // 
+            // encryption
+            // 
+            this.encryption.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encryption.Location = new System.Drawing.Point(430, 437);
+            this.encryption.Name = "encryption";
+            this.encryption.Size = new System.Drawing.Size(152, 29);
+            this.encryption.TabIndex = 6;
+            this.encryption.Text = "Зашифровать";
+            this.encryption.UseVisualStyleBackColor = true;
+            this.encryption.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(636, 6);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(572, 405);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(10, 437);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(171, 29);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(572, 405);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.richTextBox5);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.richTextBox4);
+            this.tabPage2.Controls.Add(this.richTextBox6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1259, 715);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Дешифровать";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1010, 416);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Взломанный ключ";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(1013, 435);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(171, 32);
+            this.richTextBox5.TabIndex = 7;
+            this.richTextBox5.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(426, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Дешифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(636, 6);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(572, 405);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(6, 6);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(572, 405);
+            this.richTextBox6.TabIndex = 5;
+            this.richTextBox6.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 469);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1025, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Инструкция : вводить можно текст  только с буквами  русского языка , в качестве ключ" +
+    "а можно вписывать только русские слова без лишних символов и цифр";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 538);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button encryption;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Первая_пара_тимп
+{
+    public partial class Form1 : Form
+    {
+        string s = ""; // строка для основной работы 
+
+        string ss = "";// строка для обработки от лишних символов 
+
+        string S = ""; // строка для вывода
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string key; // принимаемый ключ шифровки 
+
+        string al = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // русский алфавит 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////
+
+        /// Шифровка
+
+        ///////////////////////////////////////////////////
+
+        public bool string_in(char a, string s) // функция, которая проверяет наличие того или иного элемента в алфавите 
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (a == s[i])
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ss = richTextBox1.Text.ToLower(); // получаем текст из окошка в нижнем колонтитуле 
+
+                key = richTextBox3.Text.ToLower(); // получаем ключ из окошка 
+
+                int size_key = key.Length; // получаем длину ключа 
+
+                int[] key_shift = new int[size_key]; // массив для индексов ключа
+
+                for (int i = 0; i < ss.Length; i++) // избавляемся от лишних символов, пробелов ( получается сплошной текст)
+                {
+                    if (string_in(ss[i], al))
+                        s += ss[i];
+                }
+
+                ///
+                /////
+
+
+                for (int i = 0, j = 0; i < size_key; i++) // сдвиг по ключу 
+                {
+                    j = 0;
+                    for (; j < al.Length; j++)
+                        if (al[j] == key[i])
+                            break;
+                    key_shift[i] = j;
+
+                }
+
+                /////
+                ///
+
+                ///
+                /////
+                for (int i = 0, j = 0; i < s.Length; i++) // главный цикл, который проводит шифрование
+                {
+                    if (string_in(s[i], al))
+                    {
+                        j = 0;
+                        for (; j < al.Length; j++) // сдвиг по исходному элементу 
+                            if (al[j] == s[i])
+                                break;
+                        S += al[(j + key_shift[i % size_key]) % 33];
+                    }
+                    else
+                        S += s[i];
+                }
+
+                richTextBox2.Text = S;// вывод зашифрованного текста 
+
+                S = ""; //  очитска памяти для далнейшей с ними работы 
+
+                ss = ""; // очитска памяти для далнейшей с ними работы 
+
+                s = ""; // очитска памяти для далнейшей с ними работы 
+
+            }
+            catch
+            { MessageBox.Show("Неправильный ввод ключа или текста"); }
+            /////
+            ///
+        }
+
+        ///////////////////////////////////////////////////
+
+        /// Дешифровка 
+
+        ///////////////////////////////////////////////////
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ss = richTextBox6.Text.ToLower(); // получаем текст из окошка
+
+                string string_for_size_of_key = ""; // строка для вычисления длины ключа 
+
+                int size_key = 0; // длина ключа 
+
+                int[] N = new int[33]; // массив для частотного анализа 
+
+                double hit_index = 0; // индекс самой частой буквы
+
+                int n = 0; // количество символов в тексте 
+
+
+                for (int i = 0; i < ss.Length; i++) // // избавляемся от лишних символов, пробелов ( получается сплошной текст)
+                {
+                    if (string_in(ss[i], al))
+                        s += ss[i];
+                }
+
+
+
+                for (int p = 2; p < 20; p++) // цикл, который вычисляет длину ключа (предпологается что ключ-слово не длиннее 19 символов)
+                {
+                    n = 0;
+                    hit_index = 0;
+                    string_for_size_of_key = ""; // промежуточная строка для работы с группами символов 
+                    for (int t = 0; t < s.Length; t = t + p) // отбираем символы с некотором шагом р
+                    {
+                        if (string_in(s[t], al))
+                            string_for_size_of_key += s[t];
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+
+                    for (int i = 0; i < al.Length; i++) // цикл для подсчета  частоты букв
+                    {
+                        N[i] = 0;
+                        for (int j = 0; j < string_for_size_of_key.Length; j++)
+                            if (al[i] == string_for_size_of_key[j])
+                                N[i]++;
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+
+                    for (int i = 0; i < al.Length; i++) // цикл  для подсчета  количества символов
+                        n += N[i];
+
+
+                    ///
+                    ////////
+                    ///
+
+
+                    for (int i = 0; i < al.Length; i++) // цикл для подсчета индекса совпадений 
+                        hit_index += (double)(N[i] * (N[i] - 1)) / (n * (n - 1));
+
+
+                    ///
+                    ////////
+                    ///
+
+                    if (hit_index > 0.054) // если индекс совпадений больше критического числа => мы нашли длину ключа 
+                    {
+                        size_key = p;
+                        break;
+                    }
+                }
+
+
+                ///
+                ////////
+                ///
+
+
+                int[] index_of_key = new int[size_key]; // массив индексов ключа для дешифровки 
+
+                string decrypted_message = ""; // строка для вывода расшифрованного сообщения 
+
+                for (int y = 0; y < size_key; y++) // 
+                {
+                    int shift = 0; // индекс определенной буквы ключа 
+
+                    int quantity_top_symbol = 0; // количество самой частой буквы
+
+                    int index_top_symbol = 0; //индекс самой частой буквы
+
+                    string string_for_analyze = ""; // строка для применения дешифровки цезаря для группы символов 
+
+                    ///
+                    ////////
+                    ///
+
+                    for (int t = y; t < s.Length; t = t + size_key) // отбираем символы с некотором шагом size_key
+                    {
+                        string_for_analyze += s[t];
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+                    for (int i = 0; i < al.Length; i++) // цикл для подсчета   частоты букв
+                    {
+                        N[i] = 0;
+                        for (int j = 0; j < string_for_analyze.Length; j++)
+                            if (al[i] == string_for_analyze[j])
+                                N[i]++;
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+                    for (int i = 0; i < al.Length; i++) // цикл для подсчета  самой частой буквы
+                    {
+                        quantity_top_symbol = Math.Max(quantity_top_symbol, N[i]);
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+                    for (int i = 0; i < al.Length; i++) // цикл для подсчета  самой частой буквы
+                    {
+                        if (N[i] == quantity_top_symbol)
+                        {
+                            index_top_symbol = i;
+                            break;
+                        }
+                    }
+
+                    ///
+                    ////////
+                    ///
+
+                    shift = ((index_top_symbol - 15) + 33) % 33; // находим сдвиг для данной группы символов 
+
+                    index_of_key[y] = shift; // задаем значения для массива индексов ключа 
+                }
+
+                ////////
+                ///
+
+                for (int i = 0, j = 0; i < s.Length; i++) // дешифрование 
+                {
+                    j = 0;
+
+                    for (; j < al.Length; j++) // получаем индекс буквы алфавита
+                        if (al[j] == s[i])
+                            break;
+
+                    decrypted_message += al[((j - index_of_key[i % index_of_key.Length]) + 33) % 33];
+
+                }
+
+                richTextBox4.Text = decrypted_message; // вывод дешифрованного сообщения
+                string hak_key = "";
+                for (int i = 0; i < index_of_key.Length; i++)
+                {
+                    hak_key += al[index_of_key[i]];
+                }
+                richTextBox5.Text = hak_key;
+            }
+            catch
+            { MessageBox.Show("Неправильный ввод  текста"); }
+        }
+    }
+}
